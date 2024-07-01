e0b1196c584d41ec18eb692806f21d3980488365
Aman Welderufael <100647139+AmanWelderufael@users.noreply.github.com>
2024-07-01 15:29:19 -0400

    delete some test dependencies

--- src/test/java/com/example/TManagement/TManagementApplicationTests.java
+++ src/test/java/com/example/TManagement/TManagementApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.TManagement;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
 class TManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
